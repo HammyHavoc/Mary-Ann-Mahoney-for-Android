c962f76e9ff82c787c1cd42d4f6a4ca2755a20e2
Hammy Havoc <hammy@splitanatom.com>
2016-02-02 20:41:29 +0000

    Added About page and app title.

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ define( function ( require ) {
 		wp_ws_url : 'https://www.maryannmahoney.com/wp-appkit-api/mary-ann-mahoney-android',
 		theme : 'wpak-tabs-android',
 		version : '1.0',
-		app_title : '',
+		app_title : 'Mary Ann Mahoney',
 		debug_mode : 'off',
 		auth_key : 'Bn),r9{OWUZw[f_5^uje_p5_wg=Z+ieyg;_Jgu|QjjEd.*es[ve>;tK;,C^Q<6rQ',
 		options : {"refresh_interval":0},
