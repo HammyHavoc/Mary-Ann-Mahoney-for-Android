45da8b9e39f4aebdea73be976c45c83a180e8ea4
Hammy Havoc <hammy@splitanatom.com>
2016-02-02 19:46:53 +0000

    Initial commit.

---
+++ config.js
@@ -0,0 +1,17 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'mary-ann-mahoney-android',
+		wp_ws_url : 'https://www.maryannmahoney.com/wp-appkit-api/mary-ann-mahoney-android',
+		theme : 'wpak-tabs-android',
+		version : '1.0',
+		app_title : '',
+		debug_mode : 'off',
+		auth_key : 'Bn),r9{OWUZw[f_5^uje_p5_wg=Z+ieyg;_Jgu|QjjEd.*es[ve>;tK;,C^Q<6rQ',
+		options : {"refresh_interval":0},
+		addons : []
+	};
+
+});
