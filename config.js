38a989c581689f7506d3fa40ba4fa10fc2efca72
Hammy Havoc <hammy@splitanatom.com>
2016-07-23 21:08:49 +0100

    Updated UX.

--- config.js
+++ config.js
@@ -5,12 +5,16 @@ define( function ( require ) {
 	return {
 		app_slug : 'mary-ann-mahoney-android',
 		wp_ws_url : 'https://www.maryannmahoney.com/wp-appkit-api/mary-ann-mahoney-android',
-		theme : 'wpak-tabs-android',
-		version : '1.0',
+		wp_url : 'https://www.maryannmahoney.com',
+		theme : 'q-android',
+		version : '1.1',
 		app_title : 'Mary Ann Mahoney',
+		app_platform : 'android',
+		gmt_offset : 1,
 		debug_mode : 'off',
 		auth_key : 'Bn),r9{OWUZw[f_5^uje_p5_wg=Z+ieyg;_Jgu|QjjEd.*es[ve>;tK;,C^Q<6rQ',
 		options : {"refresh_interval":0},
+		theme_settings : [],
 		addons : []
 	};
 
